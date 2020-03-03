a6355f38af4cf34fd23f55c7063ace1e7f0b6c7a
MDaniel96 <morvai.daniel96@gmail.com>
2020-03-03 19:02:14 +0100

    added holes

---
+++ Assets/Scripts/HoleController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoleController : MonoBehaviour
+{
+    public float pullRadius;
+    public float pullForce;
+
+    public void FixedUpdate()
+    {
+        PullPlayer();
+    }
+
+    /* Pulls player in a certain radius */
+    private void PullPlayer()
+    {
+        foreach (Collider2D collider in Physics2D.OverlapCircleAll(transform.position, pullRadius))
+        {
+            if (collider.gameObject.tag == "Player")
+            {
+                Rigidbody2D player = collider.gameObject.GetComponent<Rigidbody2D>();
+                Vector3 forceDirection = transform.position - player.transform.position;
+                float strength = 1 / (forceDirection.magnitude / pullRadius);
+                player.AddForce(forceDirection * pullForce * strength);
+            }
+        }
+
+    }
+}
