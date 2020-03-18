6b8c99fed8a6bece8adda4834e445452b059a356
GyongyPeter <gyongy97@gmail.com>
2020-03-18 16:14:29 +0100

    Menu Graphic

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundManager : MonoBehaviour
+{
+
+    AudioSource audioSource;
+    public new bool enabled;
+    //Toggle toggle;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        //toggle = GetComponent<Toggle>();
+
+        enabled = true;
+        audioSource.volume = 1.0f;
+    }
+
+    void Update()
+    {
+
+        if (true)//toggle.enabled)
+        {
+            audioSource.volume = 0.5f;
+        }
+        else
+        {
+            audioSource.volume = 0.0f;
+        }
+    }
+
+}
+
